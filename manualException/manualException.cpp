55cc612d3bdb1497432544113ed2227cfd160d4d
naxsyambis <naxsyambis@gmail.com>
2024-06-04 11:00:38 +0100

    membuat manual exception dan membuat try dan catch

---
+++ manualException/manualException.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekui" << endl;
+    }
+    catch (int a)
+    {
+        //block ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
+}
+
