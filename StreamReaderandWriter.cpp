19ed766563d843ad1074c963a1f9480cac98a7f6
naxsyambis <naxsyambis@gmail.com>
2024-06-04 10:32:00 +0100

    membuat jika file ada,melakukan perulangan setiap baris, tampilkan dan menutup file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -37,6 +37,18 @@ int main()
 
     cout << endl << ">= Membuka dan membaca file " << endl;
 
+    //jika file ada maka
+    if (infile.is_open()) 
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris)) 
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n'; 
+        }
+        //tutup file tersebut setelah selesai
+        infile.close(); 
+    }
     
 }
 
