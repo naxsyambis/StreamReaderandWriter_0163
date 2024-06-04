f1f36376c60bdc3e54c0fa46e4a4d79c1b57c73a
naxsyambis <naxsyambis@gmail.com>
2024-06-04 10:41:31 +0100

    membuka file dalam menulis

--- iosFileWithPath/iosFileWithPath.cpp
+++ iosFileWithPath/iosFileWithPath.cpp
@@ -12,5 +12,13 @@ int main()
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
 
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt., ios::out");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
 }
 
